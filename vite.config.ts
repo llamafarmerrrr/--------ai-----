26f97baedcb13dbd5a5cf7d3aed330282796886b
Chenxi Jia <chenxijia@MacBook-Air.local>
2026-05-22 21:45:00 +0800

    initialt

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/--------ai-----/',
+
+
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
